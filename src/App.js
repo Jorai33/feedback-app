8651b0390c0a7073dab00e56fdf8d923840eab42
Joraï Medjo <jmedjo@pixelus.fr>
2023-10-11 15:11:28 +0200

    Conditional in JSX

--- src/App.js
+++ src/App.js
@@ -8,20 +8,30 @@ function App() {
     { id: 3, text: "Comment Three" },
   ];
 
+  const loading = false;
+  const showComment = true;
+
+  if (loading) return <h1>Loading...</h1>;
+
   return (
     // Needs to return a single enclosing tag or fragment to work
     <div className="container">
       <h1>{title.toUpperCase()}</h1>
       <p>{body}</p>
 
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => {
-            return <li key={index}>{comment.text}</li>;
-          })}
-        </ul>
-      </div>
+      {/* Ternary operator ( can also be replaced with '&&' if no else condition ) */}
+      {showComment ? (
+        <div className="comments">
+          <h3>Comments ({comments.length})</h3>
+          <ul>
+            {comments.map((comment, index) => {
+              return <li key={index}>{comment.text}</li>;
+            })}
+          </ul>
+        </div>
+      ) : (
+        "no comment"
+      )}
     </div>
   );
 }
