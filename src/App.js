238adb4f8acefbf36b1a14b041f0caa9dfd97421
Joraï Medjo <jmedjo@pixelus.fr>
2023-10-11 15:00:58 +0200

    Dynamic values and lists in JSX

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 function App() {
+  // Declare variables
+  const title = "Blog Post";
+  const body = "This is my blog post";
+  const comments = [
+    { id: 1, text: "Comment One" },
+    { id: 2, text: "Comment Two" },
+    { id: 3, text: "Comment Three" },
+  ];
+
   return (
     // Needs to return a single enclosing tag or fragment to work
     <div className="container">
-      <h1>My App</h1>
+      <h1>{title.toUpperCase()}</h1>
+      <p>{body}</p>
+
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => {
+            return <li key={index}>{comment.text}</li>;
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
