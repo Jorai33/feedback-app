da4ef6d9d36c0d5b2cda463a350edcb0e4fc15d0
Jorai33 <m.jopasi@orange.fr>
2023-10-12 12:05:13 +0200

    Form inout & State

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   // App-level state
@@ -23,6 +24,7 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
         <FeedbackStats feedbacks={feedbacks} />
         <FeedbackList feedbacks={feedbacks} handleDelete={deleteFeedback} />
       </div>
