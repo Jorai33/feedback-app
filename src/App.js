714eb33fab9ccd19be8d137ab6845f4310ab4a83
Jorai33 <m.jopasi@orange.fr>
2023-10-12 09:57:28 +0200

    Managin global state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
 
 function App() {
+  // App-level state
+  const [feedbacks, setFeedbacks] = useState(FeedbackData);
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackItem />
+        <FeedbackList feedbacks={feedbacks} />
       </div>
     </>
   );
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+  {
+    id: 1,
+    rating: 10,
+    text: "First feedback",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Second feedback",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Third feedback",
+  },
+];
+
+export default FeedbackData;
